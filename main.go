d576b05033efa8e8b8ee0b4f3bcf2fdf532960fd
AA12 <875151567@qq.com>
2022-01-08 23:52:38 +0800

    配置方案的实现

--- main.go
+++ main.go
@@ -2,17 +2,28 @@ package main
 
 import (
 	"GDForum/bootstrap"
+	"flag"
 	"fmt"
+	btsConig "GDForum/config"
+	"GDForum/pkg/config"
 	"github.com/gin-gonic/gin"
 )
-
+func init(){
+	// 加载 config 目录下的配置信息
+	btsConig.Initialize()
+}
 func main() {
+	var env string
+	flag.StringVar(&env, "env", "", "加载 .env 文件，如 --env=testing 加载的是 .env.testing 文件")
+	flag.Parse()
+	config.InitConfig(env)
 	router := gin.New()
 
 	//初始化路由绑定
 	bootstrap.SetupRoute(router)
 
-	err := router.Run(":3000")
+	// 运行服务
+	err := router.Run(":" + config.Get("app.port"))
 	if err != nil {
 		fmt.Println(err.Error())
 	}
