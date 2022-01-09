e15adcc7d18b480adc6b71ffdbad4c6d321f1757
AA12 <875151567@qq.com>
2022-01-10 01:29:18 +0800

    Email是否已注册接口

--- app/http/controllers/api/v1/auth/signup_controller.go
+++ app/http/controllers/api/v1/auth/signup_controller.go
@@ -39,3 +39,25 @@ func (sc *SignupController)IsPhoneExist(c *gin.Context){
 		"exist":user.IsPhoneExist(request.Phone),
 	})
 }
+
+//IsEmailExist 检查邮箱是否已被注册
+func (sc *SignupController)IsEmailExist(c *gin.Context){
+	request := requests.SignupEmailExistRequest{}
+	if err := c.ShouldBindJSON(&request);err != nil{
+		c.AbortWithStatusJSON(http.StatusUnprocessableEntity,gin.H{
+			"err":err.Error(),
+		})
+		fmt.Println(err.Error())
+		return
+	}
+	errs := requests.ValidateSignupEmailExist(&request,c)
+	if len(errs) > 0 {
+		c.AbortWithStatusJSON(http.StatusUnprocessableEntity, gin.H{
+			"errors": errs,
+		})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"exist": user.IsEmailExist(request.Email),
+	})
+}
