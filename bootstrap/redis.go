05dab0f6472d168867d9e23c23534e6e1c1a66bc
AA12 <875151567@qq.com>
2022-01-12 19:59:23 +0800

    解决bug

--- bootstrap/redis.go
+++ bootstrap/redis.go
@@ -7,11 +7,13 @@ import (
 )
 
 //初始化redis
-func SetupRedis(){
+func SetupRedis() {
+
+	// 建立 Redis 连接
 	redis.ConnectRedis(
-		fmt.Sprintf("%v:%v",config.GetString("redis.host"),config.GetString("redis.port")),
+		fmt.Sprintf("%v:%v", config.GetString("redis.host"), config.GetString("redis.port")),
 		config.GetString("redis.username"),
-		config.GetString("redis_password"),
-		config.GetInt("redis.port"),
-		)
+		config.GetString("redis.password"),
+		config.GetInt("redis.database"),
+	)
 }
