2ba2e438b3167a3f8d16e5cbe56cf129af689252
AA12GQ <875151567@qq.com>
2022-01-24 12:09:58 +0800

    用户列表接口

--- app/http/controllers/api/v1/users_controller.go
+++ app/http/controllers/api/v1/users_controller.go
@@ -1,6 +1,7 @@
 package v1
 
 import (
+    "GDForum/app/models/user"
     "GDForum/pkg/auth"
     "GDForum/pkg/response"
 
@@ -15,4 +16,9 @@ type UsersController struct {
 func (ctrl *UsersController) CurrentUser(c *gin.Context) {
     userModel := auth.CurrentUser(c)
     response.Data(c, userModel)
+}
+
+func (ctrl *UsersController) Index(c *gin.Context){
+    data := user.All()
+    response.Data(c,data)
 }
