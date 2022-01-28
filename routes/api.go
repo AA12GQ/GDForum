ee132ffbb98e5ca039aa8ca34cd18421f90b192d
AA12GQ <875151567@qq.com>
2022-01-28 14:01:24 +0800

    API 域名和前缀

--- routes/api.go
+++ routes/api.go
@@ -4,12 +4,19 @@ import (
 	controllers "GDForum/app/http/controllers/api/v1"
 	"GDForum/app/http/controllers/api/v1/auth"
 	"GDForum/app/http/middlewares"
+	"GDForum/pkg/config"
 	"github.com/gin-gonic/gin"
 )
 
 // RegisterAPIRoutes 注册网页相关路由
 func RegisterAPIRoutes(r *gin.Engine){
-	v1 := r.Group("/v1")
+
+	var v1 *gin.RouterGroup
+	if len(config.Get("app.api_domain")) == 0 {
+		v1 = r.Group("/api/v1")
+	} else {
+		v1 = r.Group("/v1")
+	}
 	v1.Use(middlewares.LimitIP("200-H"))
 	{
 		authGroup := v1.Group("/auth")
